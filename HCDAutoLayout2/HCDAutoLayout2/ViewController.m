e18b4de890c94fd66f9ebc33a68da88aeddaf325
黄成都 <394042583@qq.com>
2015-09-06 20:13:34 +0800

    update

---
+++ HCDAutoLayout2/HCDAutoLayout2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HCDAutoLayout2
+//
+//  Created by yifan on 15/9/6.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+#warning http://www.jianshu.com/p/3d6b2341fd83
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
